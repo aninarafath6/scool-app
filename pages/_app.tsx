1c5052e1ffe4af56547890236e5d1ceae76f858b
Shihabudheen US <sus@salesforce.com>
2022-06-25 14:32:39 +0530

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-function MyApp1({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp1;
+export default MyApp;
