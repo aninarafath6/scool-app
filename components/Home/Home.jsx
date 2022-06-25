a3f3b108e8e84149c341bd50b5e1674c22c5abd1
Shihabudheen US <sus@salesforce.com>
2022-06-25 22:13:59 +0530

    add components and elements

---
+++ components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "@elements/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button />
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,19 @@
 import Head from "next/head";
-import Button from "@components/button";
+import Home from "@components/Home";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Scool</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <Button />
+        <Home />
       </main>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
