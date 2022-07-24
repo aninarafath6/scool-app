2275022824f4dad6e605cdbda0dbcef0d3df3e45
aninarafath6 <arafathanin6@gmail.com>
2022-07-24 23:02:07 +0530

    animaiton sync

--- components/Home/Header/SideBar.jsx
+++ components/Home/Header/SideBar.jsx
@@ -3,9 +3,9 @@ import Navigation from "./Navigation";
 export default function SideBar({ toggle }) {
   return (
     <div
-      className={`h-full opacity-0  z-30 w-screen bg-fuchsia py-20 fixed top-0 lg:hidden left-0 m-0 p-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border ease-in-out duration-300    ${
-        toggle ? "visible opacity-100" : "invisible"
-      } `}
+      className={`h-full opacity-0  z-30 w-screen bg-fuchsia py-20 fixed top-0 lg:hidden left-0 m-0 p-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border ease-linear duration-${
+        toggle ? "300 " : "300 delay-500	"
+      }    ${toggle ? " opacity-100" : "opacity-0"} `}
     >
       <Navigation size="md" className="opacity-0" />
     </div>
