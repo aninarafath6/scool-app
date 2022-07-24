c81ca03d9bbb717807904bd8e50aecb5c6e8c7ff
aninarafath6 <arafathanin6@gmail.com>
2022-07-24 23:25:38 +0530

    backdrop

--- components/Home/Header/SideBar.jsx
+++ components/Home/Header/SideBar.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 export default function SideBar({ toggle }) {
   return (
     <div
-      className={` transition h-full opacity-0  z-30 w-screen bg-fuchsia py-20 fixed top-0 lg:hidden left-0 m-0 p-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border ease-linear duration-300 delay-300
+      className={` transition h-full opacity-0  z-30 w-screen bg-transparent py-20 fixed top-0 lg:hidden left-0 m-0 p-0 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 border ease-linear duration-300 delay-300
          ${toggle ? " opacity-100" : "opacity-0"} `}
     >
       <div
