29bea9f6fd6111aedf292bb5be356aafc49bc61c
aninarafath6 <arafathanin6@gmail.com>
2022-07-25 20:03:00 +0530

    optimize code🌱

--- components/Home/Header/Navigation.jsx
+++ components/Home/Header/Navigation.jsx
@@ -1,6 +1,8 @@
 import NavigationItem from "./NavigationItem";
 
 export default function Navigation({ size = "sm" }) {
+  // all navigation items
+  const navigators = ["Home", "About", "Contact", "Explore"];
   return (
     <ul
       className={`${
@@ -9,10 +11,9 @@ export default function Navigation({ size = "sm" }) {
           : "lg:flex gap-10 cursor-pointer  ease-in-out hidden  text-lg duration-300 "
       }`}
     >
-      <NavigationItem label="Home" />
-      <NavigationItem label="About" />
-      <NavigationItem label="Contact" />
-      <NavigationItem label="Explore" />
+      {navigators.map((navigation, index) => (
+        <NavigationItem key={index} label={navigation} />
+      ))}
     </ul>
   );
 }
