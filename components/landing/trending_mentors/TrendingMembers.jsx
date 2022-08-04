8c84147d17dd54feecbb3d8c0ccd286348703ba1
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-04 19:59:03 +0530

    removed doodles added

--- components/landing/trending_mentors/TrendingMembers.jsx
+++ components/landing/trending_mentors/TrendingMembers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TrendingMembers() {
   return (
     <div className=" h-screen w-screen bg-bg">
-      {/* <div className="h-24 bg-green-400 w-40">hey</div> */}
+      <div className="h-24 bg-green-400 w-40">hey</div>
     </div>
   );
 }
