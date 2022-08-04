de8a1d16534dd9e8b63de35a59fe6b326899bf1c
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-04 20:22:35 +0530

    grid image chanegd

--- components/landing/trending_mentors/TrendingMembers.jsx
+++ components/landing/trending_mentors/TrendingMembers.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function TrendingMembers() {
   return (
-    <div className=" h-screen w-screen bg-bg">
-      <div className="h-24 bg-green-400 w-40">hey</div>
+    <div className=" h-screen w-screen bg-bg block">
+      {/* <div className="h-24 bg-green-400 w-40">hey</div> */}
     </div>
   );
 }
