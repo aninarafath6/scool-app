1889cc6c770656072b9c8fa9b6ba875a2223ee1a
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-04 12:53:25 +0530

    scroll aniamtion use animtaion package integrated

--- components/landing/hero/Index.jsx
+++ components/landing/hero/Index.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import Header from "./Header/Header";
+import Lottie from "react-lottie";
+import animationData from "public/icons/Scroll down/lf30_editor_tc3rdote.json";
 
 function Hero() {
+  const animationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <div className="bg">
+    <div className="bg overflow-hidden">
       <div className="grd">
-        {" "}
         <div className="px-8">
           <Header />
+          <div className="flex flex-col  h-screen justify-evenly">
+            <div className="text-center text-slate-400  ">
+              <h1 className="text-white text-center text-4xl  md:text-6xl font-extrabold">
+                A perfectly designed
+                <br />
+                road map for everyone.{" "}
+              </h1>
+              <p className="text-sm md:text-lg mt-4 md:mt-6">
+                Our <span className="text-sky-400">expertly-designed</span> road
+                maps will help you achieve your dream.and <br />
+                Learn and grow with help from{" "}
+                <span className="text-sky-400">world-class</span> mentors.
+              </p>
+            </div>
+            <div className="">
+              {" "}
+              <Lottie
+                options={animationOptions}
+                height={50}
+                width={50}
+                speed={1.5}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
