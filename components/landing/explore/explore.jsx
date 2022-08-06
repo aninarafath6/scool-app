fdd1cca8b81729c45dc0bab06bb1e295631f2003
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-07 02:16:08 +0530

    learn like a pro section '

--- components/landing/explore/explore.jsx
+++ components/landing/explore/explore.jsx
@@ -4,7 +4,6 @@ import * as React from "react";
 export default function Explore() {
   return (
     <section className=" relative max-w-7xl mx-auto px-4 focus:outline-none sm:px-3 md:px-5 mt-20">
-     
       <div className="grid grid-cols-1 gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 max-h-[33rem] overflow-hidden mt-32">
         <ul className="space-y-8">
           <li className="text-sm leading-6">
