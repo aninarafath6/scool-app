de7cb2b3af630465c19d9b159494a2cda79e624e
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-08 01:51:34 +0530

    explore road map section with responsive

--- components/landing/explore/LearnLikePro.jsx
+++ components/landing/explore/LearnLikePro.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function LearnLikePro() {
   return (
-    <section className="text-center px-3 mt-14">
+    <section className="text-center px-9 mt-14">
       <div className="content">
         <h1 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white ">
           Learn like a pro with our
           <br /> resources
         </h1>
-        <p className="mt-6 max-w-3xl mx-auto text-lg text-slate-400">
+        <p className="mt-9 max-w-3xl mx-auto  text-lg text-slate-400">
           You can learn literally anything with our{" "}
           <span className="text-sky-400">custom-made Road Maps</span>. Which
           guides you through the steps to success,All of our maps are designed
